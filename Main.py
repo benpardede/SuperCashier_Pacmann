0b80c0bd51f19c042e93abb945de5e7d9db1c3ad
benpardede <67301642+benpardede@users.noreply.github.com>
2023-06-22 05:18:36 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+from keranjang import *
+
+
+# In[21]:
+
+
+tr_123 = Transaction()
+
+
+# In[22]:
+
+
+tr_123.add_item()
+
+
+# In[24]:
+
+
+tr_123.delete_item()
+
+
+# In[25]:
+
+
+tr_123.reset_keranjang()
+
+
+# In[27]:
+
+
+tr_123.add_item()
+
+
+# In[28]:
+
+
+tr_123.total_price()
+
+
+# In[ ]:
+
+
+
+
